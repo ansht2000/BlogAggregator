44d4b5c7786ab940c004eb6ed39918801aa951d7
ansht2000 <ansht2000@gmail.com>
2024-12-05 14:45:39 -0800

    small update, update aggregate function to actually fetch posts from followed feeds

--- handler_agg.go
+++ handler_agg.go
@@ -3,16 +3,46 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
 	"github.com/ansht2000/BlogAggregator/internal/rss"
 )
 
-func handlerAgg(s *state, cmd command) error {
-	feedUrl := "https://www.wagslane.dev/index.xml"
-	var feed *rss.RSSFeed
-	feed, err := rss.FetchFeed(context.Background(), feedUrl)
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
 		return err
 	}
-	fmt.Println(feed)
+	err = s.db.MarkFeedFetched(context.Background(), nextFeed.ID)
+	if err != nil {
+		return err
+	}
+	feed, err := rss.FetchFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		return fmt.Errorf("error fetching feed: %v", err)
+	}
+	fmt.Println(feed.Channel.Title)
+	// for _, item := range feed.Channel.Item {
+	// 	fmt.Println(item.Title)
+	// }
 	return nil
+}
+
+func handlerAgg(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.Name)
+	}
+  
+	duration, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Collecting feeds every %v\n", duration)
+	ticker := time.NewTicker(duration)
+	for ;; <-ticker.C {
+		err = scrapeFeeds(s)
+		if err != nil {
+			return err
+		}
+	}
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -10,3 +10,14 @@ SELECT name, url, user_id FROM feeds;
 
 -- name: GetFeedFromURL :one
 SELECT * FROM feeds WHERE feeds.url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * from feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
+
